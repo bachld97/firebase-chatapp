e102c122cf3e026694795eec1a83e972d80206bf
Le Duy Bach <ldbach97@gmail.com>
2018-08-28 11:08:58 +0700

    Initial Commit

---
+++ Messaging/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Messaging
+//
+//  Created by CPU12071 on 8/28/18.
+//  Copyright © 2018 Le Duy Bach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
