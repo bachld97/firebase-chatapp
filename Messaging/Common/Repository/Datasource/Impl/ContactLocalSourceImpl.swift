69eed26d66bdae8664f17a5144c693e9d8431d6f
Le Duy Bach <ldbach97@gmail.com>
2018-09-04 14:46:26 +0700

    Load image from url for ContacCell

--- Messaging/Common/Repository/Datasource/Impl/ContactLocalSourceImpl.swift
+++ Messaging/Common/Repository/Datasource/Impl/ContactLocalSourceImpl.swift
@@ -7,9 +7,13 @@ class ContactLocalSourceImpl : ContactLocalSource {
     
     func loadContacts(of user: User) -> Observable<[Contact]?> {
         var contacts = [Contact]()
-        contacts.append(Contact(userId: "ldbach", userName: "Bach Le", userAvatarUrl: nil))
+        
+        let sampleUrl1 = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Mars_sample_returnjpl.jpg/220px-Mars_sample_returnjpl.jpg"
+        let sampleUrl2 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT73Bt1VGbIq3XTzHyh-C4As0qjcYJgpjSugfCNpZIrxDwr2g2S"
+        
+        contacts.append(Contact(userId: "ldbach", userName: "Bach Le", userAvatarUrl: sampleUrl1))
         contacts.append(Contact(userId: "hello", userName: "Loo Hee", userAvatarUrl: nil))
-        contacts.append(Contact(userId: "aaa22", userName: "Um hum", userAvatarUrl: nil))
+        contacts.append(Contact(userId: "aaa22", userName: "Um hum", userAvatarUrl: sampleUrl2))
         return Observable.just(contacts)
     }
     
--- Messaging/SeeContact/ContactCell.swift
+++ Messaging/SeeContact/ContactCell.swift
@@ -20,9 +20,29 @@ class ContactCell: UITableViewCell {
         super.awakeFromNib()
         self.selectionStyle = .default
     }
-
+    
     func bind(item: ContactItem) {
         self.fullnameLabel.text = item.contact.userName
         self.usernameLabel.text = item.contact.userId
+        
+        let avaUrl = item.contact.userAvatarUrl
+        if avaUrl != nil {
+            imageFromUrl(avaUrl!)
+        }
+    }
+    
+    func imageFromUrl(_ urlString: String) {
+        if let url = URL(string: urlString) {
+            let request = URLRequest(url: url)
+            URLSession.shared.dataTask(with: request) {(data,response,error) in
+                if let imageData = data as Data? {
+                    if let img = UIImage(data: imageData) {
+                        DispatchQueue.main.async {
+                            self.avaImageView.image = img
+                        }
+                    }
+                }
+            }.resume()
+        }
     }
 }
